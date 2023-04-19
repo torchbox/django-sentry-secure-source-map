a66874b40f74671722461abc7123a77a9b6e13f6
Jake Howard <jake.howard@torchbox.com>
2023-04-19 13:00:49 +0100

    Add test to ensure incorrect token doesn't succeed

--- tests/tests.py
+++ tests/tests.py
@@ -3,6 +3,13 @@ def test_get_sourcemap(client):
     assert response.status_code == 403
 
 
+def test_get_sourcemap_with_incorrect_token(client, settings):
+    response = client.get(
+        "/static/index.js.map", HTTP_X_SENTRY_TOKEN=settings.SENTRY_SECURITY_TOKEN + "1"
+    )
+    assert response.status_code == 403
+
+
 def test_get_static_file(client):
     response = client.get("/static/index.js")
     assert response.status_code == 200
